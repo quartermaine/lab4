9335b5906363a86fc434c548e8d81a517b22a280
Zijie Feng <fengye0907@outlook.com>
2018-09-21 13:20:11 +0200

    new

--- R/linreg.R
+++ R/linreg.R
@@ -1,47 +1,50 @@
 
 linreg <- setRefClass("linreg",
   fields = list(
-    y <- "numeric",
-    X <- "data.frame",
-    y_hat <- "numeric",
-    beta <- "numeric",
-    e_hat <- "numeric",
-    df <- "numeric",
-    var_resid <- "numeric",
-    var_hat <- "numeric",
-    t_val <- "numeric"
+    y = "numeric",
+    X = "data.frame",
+    y_hat = "numeric",
+    beta = "numeric",
+    e_hat = "numeric",
+    df = "numeric",
+    var_resid = "numeric",
+    var_hat = "numeric",
+    t_val = "numeric"
   ),
   methods = list(
-    set <- function(formula, data){
+    set = function(formula, data){
       f <- as.formula(formula)
-      X <- model.matrix(f,data)
-      y <- data[,all.vars(f)[1]]
+      X <<- model.matrix(f,data)
+      y <<- data[,all.vars(f)[1]]
     },
-    coef <- function(){
+    coef = function(){
       beta <<- as.vector(solve(t((X)%*%X)%*%t(X)%*%y))
       return(beta)
     },
-    pred <- function(x){
+    pred = function(){
       y_hat <<- x%*%beta
     },
-    resid <- function(){
+    resid = function(){
       e_hat <<- y-y_hat
     },
-    df <- function(){
+    freedomdegree = function(){
       df <<- nrows(y) - ncols(X)
     },
-    residualvariance <- function(){
+    residualvariance = function(){
       var_resid <<- (t(e_hat)%*%e_hat)/df
     },
-    coeffvariance <- function(){
+    coeffvariance = function(){
       var_hat <<- var_resid%*%solve(t(X)%*%X)
     },
-    t_values <- function(){
+    t_values = function(){
       t_val <<- beta/(sqrt(var_resid))
     },
-    summary <- function(){
+    summary = function(){
       return("hhhhhh")
     }
   )
 
 )
+data <- iris
+a <- linreg$
+
