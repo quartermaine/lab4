359710cec09b097d8de58d9c97f5b0e6d1a34b47
Zijie Feng <fengye0907@outlook.com>
2018-09-21 12:56:08 +0200

    nothing new

---
+++ R/linreg.R
@@ -0,0 +1,47 @@
+
+linreg <- setRefClass("linreg",
+  fields = list(
+    y <- "numeric",
+    X <- "data.frame",
+    y_hat <- "numeric",
+    beta <- "numeric",
+    e_hat <- "numeric",
+    df <- "numeric",
+    var_resid <- "numeric",
+    var_hat <- "numeric",
+    t_val <- "numeric"
+  ),
+  methods = list(
+    set <- function(formula, data){
+      f <- as.formula(formula)
+      X <- model.matrix(f,data)
+      y <- data[,all.vars(f)[1]]
+    },
+    coef <- function(){
+      beta <<- as.vector(solve(t((X)%*%X)%*%t(X)%*%y))
+      return(beta)
+    },
+    pred <- function(x){
+      y_hat <<- x%*%beta
+    },
+    resid <- function(){
+      e_hat <<- y-y_hat
+    },
+    df <- function(){
+      df <<- nrows(y) - ncols(X)
+    },
+    residualvariance <- function(){
+      var_resid <<- (t(e_hat)%*%e_hat)/df
+    },
+    coeffvariance <- function(){
+      var_hat <<- var_resid%*%solve(t(X)%*%X)
+    },
+    t_values <- function(){
+      t_val <<- beta/(sqrt(var_resid))
+    },
+    summary <- function(){
+      return("hhhhhh")
+    }
+  )
+
+)
