e17121de6f8b5228137956c66cada69975ecd37c
Zijie Feng <fengye0907@outlook.com>
2018-09-21 15:50:53 +0200

    initialize method

--- R/linreg.R
+++ R/linreg.R
@@ -25,30 +25,6 @@ linreg <- setRefClass("linreg",
     coef = function(){
       beta <<- as.vector(solve((t(X)%*%X)%*%t(X)%*%y))
       return(beta)
-<<<<<<< HEAD
-=======
-    },
-    pred = function(){
-      y_hat <<- X%*%beta
-    },
-    resid = function(){
-      e_hat <<- y-y_hat
-    },
-    freedomdegree = function(){
-      df <<- nrows(y) - ncols(X)
-    },
-    residualvariance = function(){
-      var_resid <<- (t(e_hat)%*%e_hat)/df
-    },
-    coeffvariance = function(){
-      var_hat <<- var_resid%*%solve(t(X)%*%X)
-    },
-    t_values = function(){
-      t_val <<- beta/(sqrt(var_resid))
-    },
-    summary = function(){
-      return("hhhhhh")
->>>>>>> 95d991d4a4ba4b5a7e11e5ec2d3de95dfa8c6704
     }
   )
 
